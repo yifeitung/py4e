5f321f9490636651f08396f8a69325907c7b3644
yifeitung <yifeidong95@gmail.com>
2019-09-15 18:26:40 -0400

    Sep 15 updates

---
+++ course2_pystructure/assignment7.1.py
@@ -0,0 +1,11 @@
+# Write a program that prompts for a file name, then opens that file and reads through the file,
+# and print the contents of the file in upper case. Use the file words.txt to produce the output below.
+
+# You can download the sample data at http://www.py4e.com/code3/words.txt
+
+# Use words.txt as the file name
+fname=input('Enter the file name:')
+fh=open(fname)
+for lx in fh:
+    ly=lx.rstrip()
+    print(ly.upper()) # print the contents of the file all in upper case
